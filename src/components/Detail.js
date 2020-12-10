cf93492a7c54835439e244740a48d2366c62f2b9
gulshanyadav01 <gulshany01@gmail.com>
2020-12-10 20:03:13 +0530

    yet to come

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -56,4 +56,4 @@ const mapDispatchToProps = (dispatch) => {
     getClientById: (id) => dispatch(getClientById(id))
     };
   };
-export default connect(mapStateToProps, mapDispatchToProps)(Detail);
+export default connect(mapStateToPros, mapDispatchToProps)(Detail);
